fa1205fba1f88c6c62c39d1b62afac1b949e0e68
Derek Ahn <git.derek@gmail.com>
2017-01-10 23:52:47 -0800

    completed and submitted raindrops

--- go/raindrops/raindrops.go
+++ go/raindrops/raindrops.go
@@ -1,7 +1,32 @@
 package raindrops
 
+import "strconv"
+
 const testVersion = 2
 
-func Convert(int) string
+func Convert(n int) string {
+	var factors = make([]int, 7)
+	for i := 1; i <= n; i++ {
+		if n%i == 0 {
+			factors = append(factors, i)
+		}
+	}
 
-// The test program has a benchmark too.  How fast does your Convert convert?
+	var word string
+	for _, f := range factors {
+		switch f {
+		case 3:
+			word += "Pling"
+		case 5:
+			word += "Plang"
+		case 7:
+			word += "Plong"
+		}
+	}
+
+	if len(word) < 1 {
+		return strconv.Itoa(n)
+	}
+
+	return word
+}
