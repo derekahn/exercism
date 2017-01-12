5fd5adb9a4c5eaed05a7412ea84a0c5d7e14d20f
Derek Ahn <git.derek@gmail.com>
2017-01-12 11:29:16 -0800

    initial triangle exercise

---
+++ go/triangle/triangle.go
@@ -0,0 +1,17 @@
+package triangle
+
+const testVersion = 2
+
+// Code this function.
+func KindFromSides(a, b, c float64) Kind
+
+// Notice it returns this type.  Pick something suitable.
+type Kind
+
+// Pick values for the following identifiers used by the test program.
+NaT // not a triangle
+Equ // equilateral
+Iso // isosceles
+Sca // scalene
+
+// Organize your code for readability.
