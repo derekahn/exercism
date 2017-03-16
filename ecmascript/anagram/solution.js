47766fe82492a222da7c9de959fc5458cc819d19
Derek Ahn <git.derek@gmail.com>
2017-03-15 20:49:03 -0700

    Second iteration of es6 anagram challenge

--- ecmascript/anagram/solution.js
+++ ecmascript/anagram/solution.js
@@ -1,32 +1,18 @@
 export default class Anagram {
   constructor(word) {
-    this.word = word;
-    this.letters = word
-      .split('')
-      .reduce((obj, letter) => {
-        letter = letter.toLowerCase();
-        obj[letter] = 0;
-        return obj;
-      },
-    {});
+    this.word = word.toLowerCase();
   }
-  matches(...words) {
-    words = Array.isArray(words[0])? words[0] : words;
-    return words.map(word => {
-      let letters = 0;
-
-      word.toLowerCase().split('').forEach(letter => {
-        if (typeof this.letters[letter] !== 'undefined') {
-          ++letters;
-        }
-      });
-
-      if (letters === word.length &&
-        this.word.length === word.length &&
-        this.word.toLowerCase() !== word.toLowerCase()) {
-        return word;
-      }
-    })
-    .filter(word => word);
+  isSame(word) {
+    return this.word === word.toLowerCase();
+  }
+  isAnagram(word) {
+    return this.normalize(this.word) === this.normalize(word);
+  }
+  normalize(str) {
+    return str.toLowerCase().split('').sort().join();
+  }
+  matches(words) {
+    words = Array.isArray(words)? words : Array.from(arguments);
+    return words.filter(word => !this.isSame(word) && this.isAnagram(word));
   }
 }
