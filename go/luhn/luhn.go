7db5ab9382f348d7fa221119277f55a8c1a7f29e
Derek Ahn <git.derek@gmail.com>
2017-03-25 00:30:13 -0700

    Third iteration of go luhn challenge

--- go/luhn/luhn.go
+++ go/luhn/luhn.go
@@ -1,42 +1,21 @@
 package luhn
 
-import (
-	"regexp"
-	"strconv"
-	"unicode"
-)
+import "unicode"
 
 const testVersion = 2
 
-func Valid(cardNum string) (valid bool) {
-	valid = false
-
-	if ok := hasSymbol(cardNum); ok {
+func Valid(cardNums string) (valid bool) {
+	if hasSymbol(cardNums) {
 		return
 	}
 
-	str := normalize(cardNum)
-	if len(str) < 2 {
+	digits := extract(cardNums)
+	if len(digits) < 2 {
 		return
 	}
 
-	reversedStr := reverse(str)
-
-	nums := []string{}
-	for index, r := range reversedStr {
-		if isEven(index) {
-			nums = append(nums, properDouble(r))
-		} else {
-			nums = append(nums, string(r))
-		}
-	}
-
-	sum := 0
-	for _, str := range nums {
-		num, _ := strconv.Atoi(str)
-		sum += num
-	}
-
+	last := len(digits) - 1
+	sum := check(digits[:last]) + digits[last]
 	return sum%10 == 0
 }
 
@@ -49,42 +28,31 @@ func hasSymbol(str string) bool {
 	return false
 }
 
-func normalize(str string) string {
-	return regexp.MustCompile("[^0-9-]+").ReplaceAllString(str, "")
-}
-
-func reverse(str string) string {
-	runes := []rune(str)
-	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
-		runes[i], runes[j] = runes[j], runes[i]
-	}
-	return string(runes)
-}
-
-func isEven(i int) bool {
-	return (i+1)%2 == 0
-}
-
-func check(d []int) int {
-	for i := len(d) - 1; i >= 0; i -= 2 {
-		x := 2 * d[i]
-		if x > 9 {
-			x -= 9
+func extract(num string) (digit []int) {
+	for _, r := range num {
+		isNum := r >= '0' && r <= '9'
+		if r == ' ' || isNum {
+			if isNum {
+				digit = append(digit, int(r-'0'))
+			}
+		} else {
+			return []int{}
 		}
-		d[i] = x
 	}
-	s := 0
-	for _, x := range d {
-		s += x
-	}
-	return s
+	return
 }
 
-func properDouble(r rune) string {
-	num, _ := strconv.Atoi(string(r))
-	double := num * 2
-	if double > 9 {
-		double -= 9
+func check(digits []int) (sum int) {
+	for i := len(digits) - 1; i >= 0; i -= 2 {
+		doubled := 2 * digits[i]
+		if doubled > 9 {
+			doubled -= 9
+		}
+
+		digits[i] = doubled
 	}
-	return strconv.Itoa(double)
+	for _, x := range digits {
+		sum += x
+	}
+	return
 }
