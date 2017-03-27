d1d4d52cea9a007693cdb2b2b8e82946fbe4266a
Derek Ahn <git.derek@gmail.com>
2017-03-26 22:52:03 -0700

    First part of 1st iteration of go food_chain
    - func Song() complete; tests pass.

---
+++ go/food-chain/food_chain.go
@@ -0,0 +1,78 @@
+package foodchain
+
+import (
+	"fmt"
+	"strings"
+)
+
+const testVersion = 3
+
+var rhyme = map[string]string{
+	"fly":    "",
+	"spider": "It wriggled and jiggled and tickled inside her.\n",
+	"bird":   "How absurd to swallow a bird!\n",
+	"cat":    "Imagine that, to swallow a cat!\n",
+	"dog":    "What a hog, to swallow a dog!\n",
+	"goat":   "Just opened her throat and swallowed a goat!\n",
+	"cow":    "I don't know how she swallowed a cow!\n",
+}
+
+var animals = [8]string{
+	"fly",
+	"spider",
+	"bird",
+	"cat",
+	"dog",
+	"goat",
+	"cow",
+	"horse",
+}
+
+func Song() string {
+	verses := []string{}
+	for i, animal := range animals {
+		verse := intro(animal) + rhyme[animal] + hook(i) + end(animal)
+		verses = append(verses, verse)
+	}
+
+	return strings.Join(verses, "\n")
+}
+
+// Mechanics to create the song
+func intro(thing string) string {
+	return fmt.Sprintf("I know an old lady who swallowed a %s.\n", thing)
+}
+
+func hook(idx int) string {
+
+	spiderException := func(animal string) string {
+		if animal == "spider" {
+			return fmt.Sprintf("%s that wriggled and jiggled and tickled inside her", animal)
+		}
+		return animal
+	}
+
+	if idx < 1 || idx == len(animals)-1 {
+		return ""
+	}
+
+	stanzas := make([]string, idx)
+	for i := range stanzas {
+		stanza := fmt.Sprintf("She swallowed the %s to catch the %s.\n", animals[i+1], spiderException(animals[i]))
+		stanzas = append(stanzas, stanza)
+	}
+
+	// Reverse stanzas (slice of strings)
+	for i, j := 0, len(stanzas)-1; i < j; i, j = i+1, j-1 {
+		stanzas[i], stanzas[j] = stanzas[j], stanzas[i]
+	}
+
+	return strings.Join(stanzas, "")
+}
+
+func end(animal string) string {
+	if animal == "horse" {
+		return "She's dead, of course!"
+	}
+	return "I don't know why she swallowed the fly. Perhaps she'll die.\n"
+}
