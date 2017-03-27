5f4609181592ca5b173dd5b577b38116e278694c
Derek Ahn <git.derek@gmail.com>
2017-03-26 23:53:37 -0700

    First iteration of go largest_series_product challenge

---
+++ go/largest-series-product/largest_series_product.go
@@ -0,0 +1,44 @@
+package lsproduct
+
+import "fmt"
+
+const testVersion = 4
+
+func LargestSeriesProduct(digits string, span int) (maxProduct int64, err error) {
+	nums, err := validate(digits, span)
+	if err != nil {
+		return
+	}
+
+	for i, last := 0, len(nums)-span+1; i < last; i++ {
+		sp := int64(1)
+		for _, d := range nums[i : i+span] {
+			sp *= d
+		}
+
+		if sp > maxProduct {
+			maxProduct = sp
+		}
+	}
+	return
+}
+
+func validate(digits string, span int) (nums []int64, err error) {
+	if span < 0 {
+		err = fmt.Errorf("span is negative: %d", span)
+		return
+	}
+	if len(digits) < span {
+		err = fmt.Errorf("len(%s) < span: %d < %d", digits, len(digits), span)
+		return
+	}
+	nums = make([]int64, len(digits))
+	for i, r := range digits {
+		if r < '0' || r > '9' {
+			err = fmt.Errorf("input %q contains non-digits", digits)
+			return
+		}
+		nums[i] = int64(r - '0')
+	}
+	return
+}
