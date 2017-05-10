2e723687ad3dd10f5c3de390122712337c729cd6
Derek Ahn <git.derek@gmail.com>
2017-05-09 21:11:36 -1000

    Reformatting bracket push

--- go/bracket-push/bracket_push.go
+++ go/bracket-push/bracket_push.go
@@ -4,19 +4,15 @@ const testVersion = 4
 
 type Stack string
 
-func push(s Stack, b byte) Stack {
-	return Stack(b) + s
-}
+func push(s Stack, b byte) Stack { return Stack(b) + s }
+func pop(s Stack) (Stack, byte)  { return s[1:], s[0] }
 
-func pop(s Stack) (Stack, byte) {
-	return s[1:], s[0]
-}
-
-func Bracket(input string) (matched bool, err error) {
+func Bracket(input string) (bool, error) {
 	var stack Stack
-	var top byte
 
 	for i := 0; i < len(input); i++ {
+		var top byte
+
 		switch in := input[i]; in {
 		case '{', '[', '(':
 			stack = push(stack, in)
@@ -24,6 +20,7 @@ func Bracket(input string) (matched bool, err error) {
 			if len(stack) == 0 {
 				return false, nil
 			}
+
 			stack, top = pop(stack)
 			switch pair := string(top) + string(in); pair {
 			case "{}", "[]", "()":
