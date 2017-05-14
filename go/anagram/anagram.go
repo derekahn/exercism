b80d995d8ced44f23d0db3805ba19a449a54f41e
Derek Ahn <git.derek@gmail.com>
2017-05-14 21:56:23 +1000

    Second iteration of go anagram challenge

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -1,47 +1,27 @@
 package anagram
 
-import (
-	"sort"
-	"strings"
-)
+import "strings"
 
 const testVersion = 1
 
-func Detect(word string, list []string) (anagrams []string) {
-	list = filterList(list, word)
-	word = normalize(word)
-
-	m := formatList(list)
-	for k, v := range m {
-		if v == word {
-			anagrams = append(anagrams, k)
-		}
-	}
-	return
-}
-
-func filterList(list []string, word string) (filtered []string) {
-	for _, w := range list {
-		if len(w) == len(word) && strings.ToLower(w) != strings.ToLower(word) {
-			filtered = append(filtered, w)
-		}
-	}
-	return
-}
-
-func formatList(list []string) map[string]string {
-	m := make(map[string]string)
-	for _, w := range list {
-		key := strings.ToLower(w)
-		val := normalize(key)
-		m[key] = val
-	}
-	return m
-}
-
-func normalize(word string) string {
+func Detect(word string, candidates []string) (result []string) {
 	word = strings.ToLower(word)
-	chars := strings.Split(word, "")
-	sort.Strings(chars)
-	return strings.Join(chars, "")
+
+	for _, candidate := range candidates {
+		candidate = strings.ToLower(candidate)
+		if isAnagram(word, candidate) {
+			result = append(result, candidate)
+		}
+	}
+	return result
+}
+
+func isAnagram(word string, candidate string) bool {
+	if candidate == word || len(word) > len(candidate) {
+		return false
+	}
+	for _, r := range word {
+		candidate = strings.Replace(candidate, string(r), "", 1)
+	}
+	return candidate == ""
 }
