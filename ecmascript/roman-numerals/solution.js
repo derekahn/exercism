26fc8cc7e18ee7113233353b134e19b213e85e55
Derek Ahn <git.derek@gmail.com>
2017-05-16 17:12:20 -1000

    First iteration of es6 roman-numeral challenge

---
+++ ecmascript/roman-numerals/solution.js
@@ -0,0 +1,21 @@
+export default (number) => [
+  { number: 1000, roman:  'M' },
+  { number:  900, roman: 'CM' },
+  { number:  500, roman:  'D' },
+  { number:  400, roman: 'CD' },
+  { number:  100, roman:  'C' },
+  { number:   90, roman: 'XC' },
+  { number:   50, roman:  'L' },
+  { number:   40, roman: 'XL' },
+  { number:   10, roman:  'X' },
+  { number:    9, roman: 'IX' },
+  { number:    5, roman:  'V' },
+  { number:    4, roman: 'IV' },
+  { number:    1, roman:  'I' }
+].reduce((result, map) => {
+  while(number >= map.number) {
+    number -= map.number;
+    result += map.roman;
+  }
+  return result;
+}, '');
