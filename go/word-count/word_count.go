eeb31c46b1a7dffbef50853a3215607f2aef41b8
Derek Ahn <git.derek@gmail.com>
2017-05-16 16:44:18 -1000

    First iteration of go word-count

--- go/word-count/word_count.go
+++ go/word-count/word_count.go
@@ -1,9 +1,25 @@
 package wordcount
 
+import (
+	"regexp"
+	"strings"
+)
+
 const testVersion = 3
 
-// Use this return type.
 type Frequency map[string]int
 
-// Just implement the function.
-func WordCount(phrase string) Frequency
+func WordCount(phrase string) (freq Frequency) {
+	freq = make(Frequency, 0)
+	for _, word := range strings.Fields(normalize(phrase)) {
+		word = strings.Trim(word, "'")
+		freq[word]++
+	}
+	return
+}
+
+func normalize(phrase string) string {
+	r, _ := regexp.Compile(`[^\w|']`)
+	phrase = strings.ToLower(phrase)
+	return r.ReplaceAllLiteralString(phrase, " ")
+}
