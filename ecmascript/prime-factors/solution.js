0d95f2a8b74c6ce77f7b7ed2c918a5aa0018c535
Derek Ahn <git.derek@gmail.com>
2017-05-21 23:39:20 -0700

    First iteration of es6 prime-factors challenge

---
+++ ecmascript/prime-factors/solution.js
@@ -0,0 +1,19 @@
+export default class PrimeFactors {
+  for(num) {
+    const factors = [];
+    let currentFactor = 2;
+
+    while (num !== 1) {
+      if (num % currentFactor === 0) {
+        factors.push(currentFactor);
+
+        num /= currentFactor;
+        currentFactor  = 2;
+      } else {
+        ++currentFactor;
+      }
+    }
+
+    return factors;
+  }
+}
