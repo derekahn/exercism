42b6db03cfb394cb1066d682055363cca7033f32
Derek Ahn <git.derek@gmail.com>
2017-07-12 20:17:54 -0700

    Formatting es6 sieve challenge

--- ecmascript/sieve/solution.js
+++ ecmascript/sieve/solution.js
@@ -4,20 +4,21 @@ export default class Sieve {
     this.getPrimesUpTo(limit);
   }
 
-  getPrimesUpTo(limit) {
-    const start = 2;
-
+  getPrimesUpTo(limit, start = 2) {
     Array(start + limit)
       .fill(0)
       .map( (_, i) => i )
       .slice(start)
       .filter(i => i <= limit)
       .reduce((sieve, i) => {
+
         if (!sieve[i]) {
           this.primes.push(i);
+
           for (let j = 2 * i; j <= limit; j += i) {
-            sieve[j] = true
+            sieve[j] = true;
           }
+
         }
         return sieve;
       }, []);
