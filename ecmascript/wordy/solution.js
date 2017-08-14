18ed6980181b04c9a89e9853720c33a991c5ff5b
Derek Ahn <git.derek@gmail.com>
2017-08-13 19:10:16 -0700

    First iteration of es6 wordy

---
+++ ecmascript/wordy/solution.js
@@ -0,0 +1,35 @@
+const OPS = {
+  multiplied: '*',
+  minus: '-',
+  plus: '+',
+  divided: '/',
+};
+
+const ArgumentError = (message = 'invalid') => ({
+  name: 'argument error',
+  message,
+});
+
+class WordProblem {
+  constructor(question) {
+    this.question = this.parse(question);
+    this.hasOps   = question.match(new RegExp(/(plus|minus|divided by|multiplied by)+/g));
+  }
+  parse(question) {
+    return question.replace('?', '')
+      .split(' ')
+      .filter(word => !isNaN(+word) || Object.keys(OPS).includes(word))
+      .map(word => OPS[word] || word)
+      .join('');
+  }
+  answer() {
+    const result = eval(this.question);
+    if (!result || !this.hasOps) {
+      throw new ArgumentError();
+    }
+    return result;
+  }
+}
+
+
+export default { WordProblem, ArgumentError };
