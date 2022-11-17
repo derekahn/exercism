8461935a3156def12bd477cac97f74b8ef5b9252
Derek Ahn <git.derek@gmail.com>
2022-11-16 20:34:21 -0800

    second iteration or Resistor Color 🦀

--- rust/resistor-color/src/lib.rs
+++ rust/resistor-color/src/lib.rs
@@ -1,55 +1,44 @@
+use int_enum::IntEnum;
 use std::slice::Iter;
 
-#[derive(Debug, PartialEq, Clone, Copy, Eq)]
+#[repr(u32)]
+#[derive(Debug, PartialEq, Clone, Copy, IntEnum)]
 pub enum ResistorColor {
-    Black,
-    Brown,
-    Red,
-    Orange,
-    Yellow,
-    Green,
-    Blue,
-    Violet,
-    Grey,
-    White,
+    Black = 0,
+    Brown = 1,
+    Red = 2,
+    Orange = 3,
+    Yellow = 4,
+    Green = 5,
+    Blue = 6,
+    Violet = 7,
+    Grey = 8,
+    White = 9,
 }
 
 impl ResistorColor {
     pub fn iterator() -> Iter<'static, ResistorColor> {
         use ResistorColor::*;
 
-        static RC: [ResistorColor; 10] = [
+        static COLORS: [ResistorColor; 10] = [
             Black, Brown, Red, Orange, Yellow, Green, Blue, Violet, Grey, White,
         ];
 
-        RC.iter()
+        COLORS.iter()
     }
 }
 
 pub fn color_to_value(color: ResistorColor) -> u32 {
-    for (i, e) in ResistorColor::iterator().enumerate() {
-        if color == *e {
-            return i as u32;
-        }
-    }
-    return 0;
+    color as u32
 }
 
 pub fn value_to_color_string(value: u32) -> String {
-    match value {
-        0 => String::from("Black"),
-        1 => String::from("Brown"),
-        2 => String::from("Red"),
-        3 => String::from("Orange"),
-        4 => String::from("Yellow"),
-        5 => String::from("Green"),
-        6 => String::from("Blue"),
-        7 => String::from("Violet"),
-        8 => String::from("Grey"),
-        9 => String::from("White"),
-        _ => String::from("value out of range"),
+    match ResistorColor::from_int(value) {
+        Ok(resistor) => format!("{:?}", resistor),
+        Err(_) => String::from("value out of range"),
     }
 }
+
 pub fn colors() -> Vec<ResistorColor> {
     let mut colors: Vec<ResistorColor> = vec![];
     for &color in ResistorColor::iterator() {
