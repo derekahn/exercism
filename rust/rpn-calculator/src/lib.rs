aa0fb7adfd82ecd2d2a7c3344ad5f1da1d6ec8eb
Derek Ahn <git.derek@gmail.com>
2022-11-17 13:12:27 -0800

    second iteration of rpn-calculator 🦀
    - better handling with Some/match on stack.pop
    
    - learning the power of match statements and nested matches

--- rust/rpn-calculator/src/lib.rs
+++ rust/rpn-calculator/src/lib.rs
@@ -15,22 +15,16 @@ pub fn evaluate(inputs: &[CalculatorInput]) -> Option<i32> {
     for input in inputs {
         match input {
             Value(number) => stack.push(*number),
-            _ => {
-                if stack.len() < 2 {
-                    return None;
-                }
-
-                let b = stack.pop().unwrap_or(0);
-                let a = stack.pop().unwrap_or(0);
-
-                match input {
+            _ => match (stack.pop(), stack.pop()) {
+                (Some(b), Some(a)) => match input {
                     Add => stack.push(a + b),
                     Subtract => stack.push(a - b),
                     Multiply => stack.push(a * b),
                     Divide => stack.push(a / b),
                     _ => return None,
-                }
-            }
+                },
+                _ => return None,
+            },
         }
     }
 
