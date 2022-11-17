e698f7033e6f759dc95fb42d93b7b4cfde61fd4e
Derek Ahn <git.derek@gmail.com>
2022-11-16 20:57:38 -0800

    completed short fibonacci sequence 🦀

---
+++ rust/short-fibonacci/src/lib.rs
@@ -0,0 +1,23 @@
+/// Create an empty vector
+pub fn create_empty() -> Vec<u8> {
+    Vec::<u8>::new()
+}
+
+/// Create a buffer of `count` zeroes.
+///
+/// Applications often use buffers when serializing data to send over the network.
+pub fn create_buffer(count: usize) -> Vec<u8> {
+    vec![0; count]
+}
+
+/// Create a vector containing the first five elements of the Fibonacci sequence.
+///
+/// Fibonacci's sequence is the list of numbers where the next number is a sum of the previous two.
+/// Its first five elements are `1, 1, 2, 3, 5`.
+pub fn fibonacci() -> Vec<u8> {
+    let mut fib = vec![1, 1];
+    for i in 1..4 {
+        fib.push(fib[i] + fib[i - 1]);
+    }
+    fib
+}
