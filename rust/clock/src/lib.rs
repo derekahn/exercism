4ea5f7c02c53a0f07bc994cc39008f7a58c68437
Derek Ahn <git.derek@gmail.com>
2022-11-17 20:38:20 -0800

    second iteration of clock 🦀
    - learning about rust's algebraic methods
    
    - learning about div_euclid and rem_euclid
    
    - this is straight magic from bobahop https://exercism.org/tracks/rust/exercises/clock/solutions/bobahop

--- rust/clock/src/lib.rs
+++ rust/clock/src/lib.rs
@@ -1,5 +1,8 @@
 use std::fmt;
 
+const MINUTES_PER_DAY: i32 = 1440;
+const MINUTES_PER_HOUR: i32 = 60;
+
 #[derive(Debug, PartialEq, Eq)]
 pub struct Clock {
     minutes: i32,
@@ -8,33 +11,24 @@ pub struct Clock {
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
         Self {
-            minutes: Self::normalize((60 * hours) + minutes),
+            minutes: ((hours * MINUTES_PER_HOUR) + minutes).rem_euclid(MINUTES_PER_DAY),
         }
     }
 
     pub fn add_minutes(self, minutes: i32) -> Self {
         Self {
-            minutes: Self::normalize(self.minutes + minutes),
+            minutes: (self.minutes + minutes).rem_euclid(MINUTES_PER_DAY),
         }
-    }
-
-    pub fn hours(&self) -> i32 {
-        self.minutes / 60
-    }
-
-    pub fn minutes(&self) -> i32 {
-        self.minutes % 60
-    }
-
-    fn normalize(minutes: i32) -> i32 {
-        const MINUTES_PER_DAY: i32 = 1440;
-
-        (MINUTES_PER_DAY + (minutes % MINUTES_PER_DAY)) % MINUTES_PER_DAY
     }
 }
 
 impl fmt::Display for Clock {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "{:02}:{:02}", self.hours(), self.minutes())
+        write!(
+            f,
+            "{:02}:{:02}",
+            self.minutes / MINUTES_PER_HOUR,
+            self.minutes % MINUTES_PER_HOUR
+        )
     }
 }
