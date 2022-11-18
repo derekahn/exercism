0d89862f338d2b80e029c2fbe8bccf787b396e6f
Derek Ahn <git.derek@gmail.com>
2022-11-17 16:56:38 -0800

    second iteration of magazine-cutout 🦀
    - simplified it to not compare against 2 hashmaps

--- rust/magazine-cutout/src/lib.rs
+++ rust/magazine-cutout/src/lib.rs
@@ -2,31 +2,21 @@
 // to enable stricter warnings.
 use std::collections::HashMap;
 
-fn index_words<'a>(words: &'a [&'a str]) -> HashMap<&'a str, i32> {
+pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
     let mut map = HashMap::new();
 
-    for &word in words {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
+    for word in magazine.iter() {
+        *map.entry(word).or_insert(0) += 1;
     }
 
-    map
-}
-
-pub fn can_construct_note(magazine: &[&str], note: &[&str]) -> bool {
-    let mag_words = index_words(magazine);
-    let note_words = index_words(note);
-
-    let mut is_possible = false;
-
-    for (key, val) in note_words {
-        let mag_word_count = mag_words.get(key).unwrap_or(&0);
-        if *mag_word_count < val {
-            return false;
+    for word in note.iter() {
+        match map.get_mut(word) {
+            None | Some(0) => {
+                return false;
+            }
+            Some(x) => *x -= 1,
         }
-
-        is_possible = true
     }
 
-    is_possible
+    return true;
 }
