827cd2dc269f2efef064f79a4034c3c96569a167
Derek Ahn <git.derek@gmail.com>
2022-11-18 17:05:10 -0800

    second iteration of proverb 🦀
    - learned about .chain; appends a converges a second collection
    
    - learned about std::iter::once; creates an iterator with a single iteration/run
    
    - thanks to nicker's solution https://exercism.org/tracks/rust/exercises/proverb/solutions/nickers for teaching me

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,22 +1,13 @@
+use std::iter::once;
+
 pub fn build_proverb(list: &[&str]) -> String {
-    match list.len() {
-        0 => return String::new(),
-        1 => return format!("And all for the want of a {}.", list[0]),
-        _ => {
-            let mut phrases: Vec<String> = Vec::new();
-            let end = list.len() - 1;
+    match list.first() {
+        None => String::new(),
 
-            for (i, &word) in list.iter().enumerate() {
-                let mut phrase: String = format!("And all for the want of a {}.", list[0]);
-
-                if i != end {
-                    phrase = format!("For want of a {} the {} was lost.", word, list[i + 1]);
-                }
-
-                phrases.push(phrase);
-            }
-
-            phrases.join("\n")
-        }
+        Some(word) => list
+            .windows(2)
+            .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
+            .chain(once(format!("And all for the want of a {}.", word)))
+            .collect(),
     }
 }
