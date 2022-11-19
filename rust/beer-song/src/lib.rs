5d0a59e5c55edbf05116f4c633b3088623065f26
Derek Ahn <git.derek@gmail.com>
2022-11-19 11:41:35 -0800

    second iteration beer song 🦀
    - Attempted to use match in verse() for first iteration but struggled with interpolation
    - Used functional style in sing() inspired by justin1dennison solution https://exercism.org/tracks/rust/exercises/beer-song/solutions/justin1dennison

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,51 +1,17 @@
-fn bottles(num: u32) -> String {
-    match num {
-        0 => String::from("No more bottles"),
-        1 => String::from("1 bottle"),
-        _ => format!("{num} bottles"),
-    }
-}
-
 pub fn verse(n: u32) -> String {
-    let line1 = format!("{} of beer on the wall, ", bottles(n));
-    let line2 = format!("{} of beer.\n", bottles(n).to_lowercase());
+    let bottle = if n == 2 { "bottle" } else { "bottles" };
 
-    let line3 = if n == 0 {
-        String::from("Go to the store and buy some more, ")
-    } else {
-        let subject = if n == 1 {
-            String::from("it")
-        } else {
-            String::from("one")
-        };
-        format!("Take {subject} down and pass it around, ")
-    };
-
-    let line4 = {
-        let remainder = if n == 0 { 99 } else { n - 1 };
-
-        format!(
-            "{} of beer on the wall.\n",
-            bottles(remainder).to_lowercase()
-        )
-    };
-
-    format!("{line1}{line2}{line3}{line4}")
+    match n {
+        0 => String::from("No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"),
+        1 => String::from("1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n"),
+        _ => format!("{n} bottles of beer on the wall, {n} bottles of beer.\nTake one down and pass it around, {} {bottle} of beer on the wall.\n", n - 1),
+    }
 }
 
 pub fn sing(start: u32, end: u32) -> String {
-    let mut verses: Vec<String> = Vec::new();
-    let mut i = start;
-
-    while i >= end {
-        verses.push(verse(i));
-
-        if i == 0 {
-            break;
-        }
-
-        i -= 1;
-    }
-
-    verses.join("\n")
+    (end..=start)
+        .rev()
+        .map(|n| verse(n))
+        .collect::<Vec<String>>()
+        .join("\n")
 }
