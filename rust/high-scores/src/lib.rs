cde10d365224201aab64280bfebbea41f09a65be
Derek Ahn <git.derek@gmail.com>
2022-11-19 14:48:57 -0800

    second iteration high-scores 🦀
    - learned about .to_vec(), .iter().max(), .iter().last(), .sort_unstable(), and .take()

--- rust/high-scores/src/lib.rs
+++ rust/high-scores/src/lib.rs
@@ -13,26 +13,16 @@ impl<'a> HighScores<'a> {
     }
 
     pub fn latest(&self) -> Option<u32> {
-        self.scores().to_vec().pop()
+        self.scores().iter().last().cloned()
     }
 
     pub fn personal_best(&self) -> Option<u32> {
-        self.scores()
-            .iter()
-            .map(|&score| score)
-            .reduce(|top, score| if top >= score { top } else { score })
+        self.scores().iter().max().cloned()
     }
 
     pub fn personal_top_three(&self) -> Vec<u32> {
-        self.scores().iter().fold(Vec::new(), |mut accum, &n| {
-            if accum.len() < 3 {
-                accum.push(n);
-            } else if n > accum[2] {
-                accum.pop();
-                accum.push(n);
-            }
-            accum.sort_by(|a, b| b.cmp(a));
-            accum
-        })
+        let mut top_3 = self.scores().to_vec();
+        top_3.sort_unstable();
+        top_3.into_iter().rev().take(3).collect()
     }
 }
