52a436895aca0d9a1835c0703a22b1260ef5d517
Derek Ahn <git.derek@gmail.com>
2022-11-18 18:04:48 -0800

    third iteration of proverb 🦀
    - learned about zip()
    
    - learned about iter().skip(n); skips over n elements
    
    - learned about string concat with +
    
    - thanks to danstrother's solution https://exercism.org/tracks/rust/exercises/proverb/solutions/danstrother

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,13 +1,11 @@
-use std::iter::once;
-
 pub fn build_proverb(list: &[&str]) -> String {
-    match list.first() {
-        None => String::new(),
-
-        Some(word) => list
-            .windows(2)
-            .map(|w| format!("For want of a {} the {} was lost.\n", w[0], w[1]))
-            .chain(once(format!("And all for the want of a {}.", word)))
-            .collect(),
+    if list.is_empty() {
+        return String::new();
     }
+
+    list.iter()
+        .zip(list.iter().skip(1))
+        .map(|(&w1, &w2)| format!("For want of a {} the {} was lost.\n", w1, w2))
+        .collect::<String>()
+        + &format!("And all for the want of a {}.", list[0])
 }
