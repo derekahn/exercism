e0fae427f24b63c4dc9062c6e1b1f2c4ad96f6a9
Derek Ahn <git.derek@gmail.com>
2022-11-19 18:13:47 -0800

    second iteration sum_of_multiples 🦀
    - found the .some() JS equivalent, it's .any()
    
    - also learned about .sum()

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,11 +1,5 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
     (1..limit)
-        .filter(|&a| {
-            !factors
-                .iter()
-                .filter(|&b| *b > 0 && a % b == 0)
-                .collect::<Vec<&u32>>()
-                .is_empty()
-        })
-        .fold(0, |sum, n| sum + n)
+        .filter(|n| factors.iter().any(|&m| m != 0 && n % m == 0))
+        .sum()
 }
