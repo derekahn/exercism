aef9d68434e89f3d255ce998b70b545fa37b076f
Derek Ahn <git.derek@gmail.com>
2022-11-25 17:43:11 -0800

    second iteration 🦀
    - learned about passing structs; &str.contains(char::_METHOD_) 🤯
    
    - learned about &str.ends_with method

--- rust/bob/src/lib.rs
+++ rust/bob/src/lib.rs
@@ -1,35 +1,11 @@
 pub fn reply(message: &str) -> &str {
-    let msg = message.trim();
-    if msg.len() <= 1 {
-        return "Fine. Be that way!";
+    let is_yelling = |m: &str| m.contains(char::is_alphabetic) && !m.contains(char::is_lowercase);
+
+    match message.trim() {
+        m if m.is_empty() => "Fine. Be that way!",
+        m if m.ends_with("?") && is_yelling(m) => "Calm down, I know what I'm doing!",
+        m if m.ends_with("?") => "Sure.",
+        m if is_yelling(m) => "Whoa, chill out!",
+        _ => "Whatever.",
     }
-
-    let has_letters = get_letters(msg).iter().any(|c| c.is_alphabetic());
-
-    let is_yelling = get_letters(msg)
-        .iter()
-        .filter(|c| c.is_alphabetic())
-        .all(|c| c.is_uppercase());
-
-    let is_question: bool = match get_letters(msg).pop() {
-        Some(c) if c == '?' => true,
-        _ => false,
-    };
-
-    if is_yelling && is_question && has_letters {
-        return "Calm down, I know what I'm doing!";
-    }
-    if is_yelling && has_letters {
-        return "Whoa, chill out!";
-    }
-    if is_question {
-        return "Sure.";
-    }
-    "Whatever."
-}
-
-fn get_letters(msg: &str) -> Vec<char> {
-    msg.chars()
-        .filter(|c| c.is_alphabetic() || *c == '?')
-        .collect()
 }
