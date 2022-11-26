0190c165f82c9b04aaf3cd43df1a667c83f6e274
Derek Ahn <git.derek@gmail.com>
2022-11-25 19:29:19 -0800

    third iteration of acronym 🦀
    - learned about iter.flat_map
    
    - the power of .chain()

--- rust/acronym/src/lib.rs
+++ rust/acronym/src/lib.rs
@@ -1,19 +1,14 @@
 pub fn abbreviate(phrase: &str) -> String {
     phrase
         .split(|c: char| c.is_whitespace() || c == '-' || c == '_')
-        .filter(|word| !word.is_empty())
-        .fold(String::new(), |mut accum, word| {
+        .flat_map(|word| {
             let inner_upper_letters = word
                 .chars()
                 .skip_while(|c| c.is_uppercase())
                 .filter(|c| c.is_uppercase());
 
-            word.chars()
-                .take(1)
-                .chain(inner_upper_letters)
-                .for_each(|c| accum.push(c));
-
-            accum
+            word.chars().take(1).chain(inner_upper_letters)
         })
+        .collect::<String>()
         .to_uppercase()
 }
