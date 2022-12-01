71db26510e3f1827d7d900c6d0d33edcc71e25f5
Derek Ahn <git.derek@gmail.com>
2022-12-01 15:38:55 -0800

    second iteration of luhn 🦀
    - used match statement
    
    - remembered modulus returns same as long as it's less than

--- rust/luhn/src/lib.rs
+++ rust/luhn/src/lib.rs
@@ -1,25 +1,19 @@
 /// Check a Luhn checksum.
 pub fn is_valid(code: &str) -> bool {
     let seq = code.replace(" ", "");
+
     if seq.len() < 2 || !seq.chars().all(char::is_numeric) {
         return false;
     }
 
-    let double = |n: u32| if n * 2 > 9 { n * 2 - 9 } else { n * 2 };
-
     seq.chars()
         .rev()
-        .filter(|c| c.is_ascii_digit())
+        .filter_map(|c| c.to_digit(10))
         .enumerate()
-        .map(|(i, n)| match n.to_digit(10) {
-            Some(num) => {
-                if i % 2 == 1 {
-                    double(num)
-                } else {
-                    num
-                }
-            }
-            None => 0,
+        .map(|(i, n)| match i % 2 {
+            0 => n,
+            _ if n == 9 => n,
+            _ => (n * 2) % 9,
         })
         .sum::<u32>()
         % 10
