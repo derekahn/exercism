c74fabb17a106b7138b63b9b652ab8a7ac6e47c1
Derek Ahn <git.derek@gmail.com>
2022-12-31 10:22:35 -0800

    second iteration space-age 🦀

--- rust/space-age/src/lib.rs
+++ rust/space-age/src/lib.rs
@@ -16,36 +16,20 @@ pub trait Planet {
     }
 }
 
-pub struct Mercury;
-pub struct Venus;
-pub struct Earth;
-pub struct Mars;
-pub struct Jupiter;
-pub struct Saturn;
-pub struct Uranus;
-pub struct Neptune;
+macro_rules! planet {
+    ($i:ident, $e:expr) => {
+        pub struct $i;
+        impl Planet for $i {
+            const ORBIT: f64 = $e;
+        }
+    };
+}
 
-impl Planet for Mercury {
-    const ORBIT: f64 = 0.2408467;
-}
-impl Planet for Venus {
-    const ORBIT: f64 = 0.61519726;
-}
-impl Planet for Earth {
-    const ORBIT: f64 = 1.0;
-}
-impl Planet for Mars {
-    const ORBIT: f64 = 1.8808158;
-}
-impl Planet for Jupiter {
-    const ORBIT: f64 = 11.862615;
-}
-impl Planet for Saturn {
-    const ORBIT: f64 = 29.447498;
-}
-impl Planet for Uranus {
-    const ORBIT: f64 = 84.016846;
-}
-impl Planet for Neptune {
-    const ORBIT: f64 = 164.79132;
-}
+planet!(Earth, 1.0);
+planet!(Mercury, 0.2408467);
+planet!(Venus, 0.61519726);
+planet!(Mars, 1.8808158);
+planet!(Jupiter, 11.862615);
+planet!(Saturn, 29.447498);
+planet!(Uranus, 84.016846);
+planet!(Neptune, 164.79132);
