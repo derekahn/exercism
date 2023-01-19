d28afabb72c99c4ff13777a6e42100b125ebb5bd
Derek Ahn <git.derek@gmail.com>
2023-01-19 15:35:53 -0800

    first iteration of allergies 🦀

---
+++ rust/allergies/src/lib.rs
@@ -0,0 +1,53 @@
+pub struct Allergies(u32);
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Allergen {
+    Eggs,
+    Peanuts,
+    Shellfish,
+    Strawberries,
+    Tomatoes,
+    Chocolate,
+    Pollen,
+    Cats,
+}
+
+impl Allergies {
+    pub fn new(score: u32) -> Self {
+        Self(score)
+    }
+
+    pub fn is_allergic_to(&self, allergen: &Allergen) -> bool {
+        let score = self.get_allergen(allergen);
+        self.0 & score > 0
+    }
+
+    fn get_allergen(&self, allergen: &Allergen) -> u32 {
+        match *allergen {
+            Allergen::Eggs => 1,
+            Allergen::Peanuts => 2,
+            Allergen::Shellfish => 4,
+            Allergen::Strawberries => 8,
+            Allergen::Tomatoes => 16,
+            Allergen::Chocolate => 32,
+            Allergen::Pollen => 64,
+            Allergen::Cats => 128,
+        }
+    }
+
+    pub fn allergies(&self) -> Vec<Allergen> {
+        vec![
+            Allergen::Eggs,
+            Allergen::Peanuts,
+            Allergen::Shellfish,
+            Allergen::Strawberries,
+            Allergen::Tomatoes,
+            Allergen::Chocolate,
+            Allergen::Pollen,
+            Allergen::Cats,
+        ]
+        .into_iter()
+        .filter(|a| self.is_allergic_to(a))
+        .collect()
+    }
+}
