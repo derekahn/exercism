ba6d8803346e9ae3229c9974b057162b405e7651
Derek Ahn <git.derek@gmail.com>
2023-01-24 13:14:50 -0800

    second iteration of grade-school 🦀

--- rust/grade-school/src/lib.rs
+++ rust/grade-school/src/lib.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 // This annotation prevents Clippy from warning us that `School` has a
 // `fn new()` with no arguments, but doesn't implement the `Default` trait.
 //
@@ -5,26 +7,25 @@
 // case, we want to keep things relatively simple. The `Default` trait is not the point
 // of this exercise.
 #[allow(clippy::new_without_default)]
+#[derive(Default)]
 pub struct School {
-    students: Vec<(u32, String)>,
+    roster: HashMap<u32, Vec<String>>,
 }
 
 impl School {
     pub fn new() -> School {
-        Self {
-            students: Vec::new(),
-        }
+        Default::default()
     }
 
     pub fn add(&mut self, grade: u32, student: &str) {
-        self.students.push((grade, student.into()))
+        let grade = self.roster.entry(grade).or_insert(Vec::new());
+        grade.push(student.to_string());
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        let mut request: Vec<u32> = self.students.clone().into_iter().map(|(g, _)| g).collect();
-        request.dedup();
-        request.sort();
-        request
+        let mut grades: Vec<u32> = self.roster.keys().map(|&grade| grade).collect();
+        grades.sort();
+        grades
     }
 
     // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
@@ -32,15 +33,8 @@ impl School {
     // the internal structure can be completely arbitrary. The tradeoff is that some data
     // must be copied each time `grade` is called.
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        let mut request: Vec<String> = self
-            .students
-            .clone()
-            .into_iter()
-            .filter(|s| s.0 == grade)
-            .map(|(_, s)| s)
-            .collect();
-
-        request.sort();
-        request
+        let mut students = self.roster.get(&grade).cloned().unwrap_or_default();
+        students.sort();
+        students
     }
 }
