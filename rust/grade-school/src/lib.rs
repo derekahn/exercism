0a2ff373a25bdfffc7c93803e7e63ffc5c353ae3
Derek Ahn <git.derek@gmail.com>
2023-01-25 10:49:23 -0800

    third iteration of grade-school 🦀

--- rust/grade-school/src/lib.rs
+++ rust/grade-school/src/lib.rs
@@ -1,16 +1,8 @@
-use std::collections::HashMap;
-
-// This annotation prevents Clippy from warning us that `School` has a
-// `fn new()` with no arguments, but doesn't implement the `Default` trait.
-//
-// Normally, it's good practice to just do what Clippy tells you, but in this
-// case, we want to keep things relatively simple. The `Default` trait is not the point
-// of this exercise.
 #[allow(clippy::new_without_default)]
+use std::collections::BTreeMap;
+
 #[derive(Default)]
-pub struct School {
-    roster: HashMap<u32, Vec<String>>,
-}
+pub struct School(BTreeMap<u32, Vec<String>>);
 
 impl School {
     pub fn new() -> School {
@@ -18,14 +10,14 @@ impl School {
     }
 
     pub fn add(&mut self, grade: u32, student: &str) {
-        let grade = self.roster.entry(grade).or_insert(Vec::new());
-        grade.push(student.to_string());
+        self.0
+            .entry(grade)
+            .and_modify(|students| students.push(String::from(student)))
+            .or_insert(vec![String::from(student)]);
     }
 
     pub fn grades(&self) -> Vec<u32> {
-        let mut grades: Vec<u32> = self.roster.keys().map(|&grade| grade).collect();
-        grades.sort();
-        grades
+        self.0.keys().copied().collect()
     }
 
     // If `grade` returned a reference, `School` would be forced to keep a `Vec<String>`
@@ -33,8 +25,13 @@ impl School {
     // the internal structure can be completely arbitrary. The tradeoff is that some data
     // must be copied each time `grade` is called.
     pub fn grade(&self, grade: u32) -> Vec<String> {
-        let mut students = self.roster.get(&grade).cloned().unwrap_or_default();
-        students.sort();
-        students
+        match self.0.get(&grade) {
+            Some(students) => {
+                let mut students = students.iter().map(|s| s.clone()).collect::<Vec<String>>();
+                students.sort();
+                students
+            }
+            None => vec![],
+        }
     }
 }
