cc1d4a3181770d2fdab59fa38d173303be41b3a9
Derek Ahn <git.derek@gmail.com>
2023-02-18 14:09:59 -0800

    second iteration pangram 🦀
    - learned how to use filter_map

--- rust/pangram/src/lib.rs
+++ rust/pangram/src/lib.rs
@@ -1,10 +1,12 @@
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
     let mut ascii_codes: Vec<u8> = sentence
-        .to_lowercase()
         .chars()
-        .map(|c| c as u8)
-        .filter(|&c| c > 96 && c < 123)
+        .filter_map(|c| match c {
+            'a'..='z' => Some(c as u8),
+            'A'..='Z' => Some(c.to_ascii_lowercase() as u8),
+            _ => None,
+        })
         .collect();
 
     ascii_codes.sort();
