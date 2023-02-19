bd385fc17cb518bd156cd8acb01aadb439152884
Derek Ahn <git.derek@gmail.com>
2023-02-19 15:19:49 -0800

    second iteration of isogram 🦀

--- rust/isogram/src/lib.rs
+++ rust/isogram/src/lib.rs
@@ -1,23 +1,11 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn check(candidate: &str) -> bool {
-    let map: HashMap<char, u8> = candidate
+    let mut set = HashSet::new();
+
+    candidate
         .to_lowercase()
         .chars()
-        .into_iter()
-        .filter_map(|c| match c {
-            'a'..='z' => Some(c),
-            _ => None,
-        })
-        .fold(HashMap::<char, u8>::new(), |mut acc, c| {
-            *acc.entry(c).or_insert(0) += 1;
-            acc
-        });
-
-    for (_, val) in map {
-        if val > 1 {
-            return false;
-        }
-    }
-    return true;
+        .filter(|c| c.is_alphabetic())
+        .all(|c| set.insert(c))
 }
